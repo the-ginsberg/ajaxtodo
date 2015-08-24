3175f7084bd99141f632d5b9a9dc38c485bceddb
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-23 22:25:52 -0400

    final commit

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,9 +1,14 @@
 function ready(){
+  $('#task_due_date').datepicker(
+      { dateFormat: "yy-mm-dd" }
+    );
+
   $('#new_task').on("ajax:success", function(event, data){
     console.log("Success!");
     // window.location.reload();
     // console.log(arguments);
     $('#task_name').val("");
+    $('#task_due_date').val("");
     $('body').append(data);
   }).on("ajax:error", function(){
     console.log("Error!");
